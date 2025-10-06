922dafc37fe0117b6bd63b0b112ed6dded17510f
denfad <denfad2003@mail.ru>
2025-10-06 18:07:39 +0300

    Adding support of upstream DNS server

--- src/main/java/service/Handler.java
+++ src/main/java/service/Handler.java
@@ -3,11 +3,15 @@ package service;
 import common.DNSMessage;
 import common.DNSRecord;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.List;
 import java.util.Optional;
 
 public class Handler {
     private final List<DNSRecord> records;
+    private static final String UPSTREAM_DNS = "8.8.8.8";
 
     public Handler(List<DNSRecord> records) {
         this.records = records;
@@ -18,12 +22,21 @@ public class Handler {
         DNSMessage requestMessage = DNSMessage.createFromByteArray(requestData);
         // ответ на запрос
         DNSMessage responseMessage = processQuery(requestMessage);
+        // если не нашли у себя домен, то перенаправляем к вышестоящему серверу
+        if(responseMessage.getAnswerCount() == 0) {
+            try {
+                return findInUpStream(requestData);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
         return responseMessage.toByteArray();
     }
 
     private DNSMessage processQuery(DNSMessage requestMessage) {
         // ищем запись
         Optional<DNSRecord> record = findValue(requestMessage);
+        System.out.println("Request: " + requestMessage.getQuestionName());
 
         // формируем пустой ответ
         DNSMessage response = DNSMessage.createEmptyResponseFromRequest(requestMessage);
@@ -32,6 +45,26 @@ public class Handler {
         record.ifPresent(response::addAnswer);
 
         return response;
+    }
+
+    private byte[] findInUpStream(byte[] requestData) throws Exception{
+        // Создаем сокет для отправки запроса к вышестоящему DNS
+        DatagramSocket upstreamSocket = new DatagramSocket();
+        upstreamSocket.setSoTimeout(5000); // Таймаут 5 секунд
+
+        // Отправляем запрос к вышестоящему DNS-серверу
+        InetAddress upstreamAddress = InetAddress.getByName(UPSTREAM_DNS);
+        DatagramPacket upstreamRequest = new DatagramPacket(
+                requestData, requestData.length, upstreamAddress, 53
+        );
+        upstreamSocket.send(upstreamRequest);
+
+        // Получаем ответ от вышестоящего DNS-сервера
+        byte[] responseData = new byte[1024];
+        DatagramPacket upstreamResponse = new DatagramPacket(responseData, responseData.length);
+        upstreamSocket.receive(upstreamResponse);
+
+        return responseData;
     }
 
     private Optional<DNSRecord> findValue(DNSMessage requestMessage) {
